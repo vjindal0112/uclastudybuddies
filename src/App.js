2a6276f490138593939d709b5226bf68abf6451c
Varun Jindal <vjindal0112@gmail.com>
2020-09-14 00:43:08 -0400

    fix GA tracking ID

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 import ReactGA from 'react-ga';
 
-const trackingId = "UA-176116817-2"; // Replace with your Google Analytics tracking ID
+const trackingId = "UA-176116817-3"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
 ReactGA.pageview(window.location.pathname + window.location.search);
 
