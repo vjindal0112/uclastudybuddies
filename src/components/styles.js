5ddd96820fd175aa77ed5fbb53ca1f7d6e6559c6
formans <formans@umich.edu>
2020-09-27 17:28:10 -0400

    all uchicago changes 1,2,3 and wording and button colors

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,5 +1,5 @@
-import styled, { keyframes } from 'styled-components'
-import { collegeDark, collegeLight } from '../constants'
+import styled, { keyframes } from "styled-components";
+import { collegeDark, collegeLight } from "../constants";
 
 export const Heading = styled.h1`
   font-size: 60px;
@@ -12,18 +12,18 @@ export const Heading = styled.h1`
 `;
 
 export const Button = styled.a`
-  border: 3px solid ${collegeLight};
-  background-color: ${collegeLight};
+  border: 3px solid ${collegeDark};
+  background-color: ${collegeDark};
   padding: 12px;
   margin: 24px;
-  color: ${collegeDark};
+  color: #fafafa;
   border-radius: 4px;
   box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
 
   transition: all 0.5s;
   &:hover {
-    border: 3px solid #EFC100;
-    background-color: #EFC100;
+    border: 3px solid #5290be;
+    background-color: #5290be;
     color: #fafafa;
   }
 `;
@@ -56,19 +56,19 @@ export const Input = styled.input`
 `;
 
 export const QuestionButton = styled.button`
-  border: 3px solid ${collegeLight};
-  background-color: ${collegeLight};
+  border: 3px solid ${collegeDark};
+  background-color: ${collegeDark};
   padding: 4px 8px;
   margin: 10px;
-  color: ${collegeDark};
+  color: #fafafa;
   font-size: 18px;
   border-radius: 4px;
   box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.2);
 
   transition: all 0.5s;
   &:hover {
-    border: 3px solid #EFC100;
-    background-color: #EFC100;
+    border: 3px solid #5290be;
+    background-color: #5290be;
   }
 `;
 
@@ -97,27 +97,27 @@ const drop = keyframes`
 
 export const SaveBanner = styled.div`
   position: fixed;
-  top:0%;
+  top: 0%;
   width: 100%;
   background-color: ${collegeLight};
   color: ${collegeDark};
   font-size: 18px;
-  height: ${props => props.animate ? "40px" : "0px"};
+  height: ${(props) => (props.animate ? "40px" : "0px")};
   display: flex;
   justify-content: center;
   align-items: center;
   font-weight: 500;
-  transition: all 2s cubic-bezier(0,1.01,.4,.99);
-  opacity: ${props => props.animate ? "1" : "0"};
+  transition: all 2s cubic-bezier(0, 1.01, 0.4, 0.99);
+  opacity: ${(props) => (props.animate ? "1" : "0")};
   z-index: 3;
 `;
 
 const Banner = styled.div`
   position: absolute;
-  top:0%;
+  top: 0%;
   width: 100%;
   background-color: ${collegeLight};
-  color: #00274C;
+  color: #00274c;
   height: 40px;
   display: flex;
   justify-content: center;
@@ -125,7 +125,7 @@ const Banner = styled.div`
   font-weight: 600;
   animation: ${drop} 6s ease-out;
   opacity: 0;
-`
+`;
 export const UserCount = styled.div`
   margin: 0px 8px;
   color: ${collegeLight};
